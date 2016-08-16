786b3100af108cc21b9b6b7240493af0e819c765
Ted <wtedw@yahoo.com>
2016-08-15 22:07:15 -0500

    You know every since a freak accident where I lost this entire repository, I'm ever committed to using git

--- app/src/main/java/net/tedwong/ping/MainActivity.java
+++ app/src/main/java/net/tedwong/ping/MainActivity.java
@@ -6,28 +6,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
+/*
+* No idea where to put this
+* Generate app icon by romannurik is licensed under CC BY 2.0
+*/
 public class MainActivity extends Activity {
     EditText ring;
     EditText front;
